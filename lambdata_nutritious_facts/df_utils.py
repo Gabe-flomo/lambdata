29462bcc2a386ba61d74654157c97a0301e2a4d9
Gabe <44428182+Nutritiousfacts@users.noreply.github.com>
2019-10-01 13:36:05 -0500

    updated the df_utils file

--- lambdata_nutritious_facts/df_utils.py
+++ lambdata_nutritious_facts/df_utils.py
@@ -4,4 +4,36 @@ utility for working with DataFrames
 
 import pandas as pd
 
-DF = pd.DataFrame([1,2,3])
+def get_data():
+    print("Options")
+    print("1) URL")
+    print("2) file upload")
+    print("3) use dummy data")
+    ans = eval(input("Choose an option by selecting the corresponding number: "))
+    print()
+
+    if ans == 1:
+        url = input("enter the URL: ")
+        df = pd.read_csv(url)
+        return df
+    elif ans == 2:
+        import os
+        path = input("Provide the path to the directory where the file is located: ")
+        os.chdir(path)
+        filename = input("Enter the file name: ")
+        df = pd.read_csv(filename)
+        return df
+    elif ans == 3:
+        df = pd.DataFrame({'A': ['a', 'b', 'a'], 'B': ['b', 'a', 'c'],
+                   'C': [1, 2, 3]})
+        return df
+    
+def remove_Nan(df):
+    """
+    this function removes nans from a dataframe
+    """
+    if df.isnull().sum() == 0:
+        print("there were no null values")
+    else:
+        df = df.dropna()
+        return df
